baa2613ecf328cde23913d03d664fa8f81fa16a6
François Rozet <francois.rozet@outlook.com>
2024-09-12 15:32:45 -0400

    Add isotropic power spectrum

--- lpdm/data.py
+++ lpdm/data.py
@@ -1,10 +1,18 @@
 r"""Data and datasets helpers."""
 
+__all__ = [
+    "field_preprocess",
+    "field_postprocess",
+    "get_well_dataset",
+    "isotropic_power_spectrum",
+]
+
+import math
 import os
 import torch
 
 from torch import Tensor
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 
 try:
     from the_well.benchmark.data.datasets import GenericWellDataset
@@ -107,3 +115,52 @@ def get_well_dataset(
         use_normalization=False,
         **kwargs,
     )
+
+
+def isotropic_power_spectrum(
+    x: Tensor,
+    edges: Optional[Tensor] = None,
+    spatial: int = 2,
+) -> Tuple[Tensor, Tensor]:
+    r"""Computes the isotropic power spectrum of a field.
+
+    Adapted from :func:`the_well.benchmark.metrics.spectral.power_spectrum`.
+
+    Arguments:
+        x: A field tensor, with shape :math:`(*, H_1, ..., H_N,)`.
+        edges: The wavelength bin edges, with shape :math:`(L + 1)`.
+        spatial: The number of spatial dimensions :math:`N`.
+
+    Returns:
+        The power spectrum and the wavelength bin edges, with shape :math:`(*, L)` and
+        :math:`(L + 1)`, respectively.
+    """
+
+    # Wavelength
+    w2_iso = 0
+
+    for i in range(spatial):
+        w_i = torch.fft.fftfreq(x.shape[-i - 1], dtype=x.dtype, device=x.device)
+        w_i = w_i.reshape([1] * (spatial - i - 1) + [-1] + [1] * i)
+        w2_iso = w2_iso + w_i**2
+
+    w_iso = torch.sqrt(w2_iso)
+
+    if edges is None:
+        bins = math.ceil(math.sqrt(max(w_iso.shape)))
+        edges = torch.linspace(0, w_iso.max(), bins + 1)
+    else:
+        bins = len(edges) - 1
+
+    indices = torch.clip(torch.bucketize(w_iso.flatten(), edges), max=bins - 1)
+    counts = torch.bincount(indices, minlength=bins)
+
+    # Power spectrum
+    p = torch.fft.fftn(x, dim=tuple(range(-spatial, 0))).abs().square()
+    p = torch.flatten(p, -spatial)
+
+    p_iso = torch.zeros((*p.shape[:-1], bins), dtype=x.dtype, device=x.device)
+    p_iso = p_iso.scatter_add(dim=-1, index=indices.expand_as(p), src=p)
+    p_iso = p_iso / torch.clip(counts, min=1)
+
+    return p_iso, edges
