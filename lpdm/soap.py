180961d7da66d51854d17325034fd8e28c12f8ce
François Rozet <francois.rozet@outlook.com>
2025-02-11 22:09:01 +0100

    Optimize SOAP

--- lpdm/soap.py
+++ lpdm/soap.py
@@ -28,8 +28,7 @@ class SOAP(torch.optim.Optimizer):
     ):
         defaults = {
             "lr": lr,
-            "betas": (betas[0], betas[1]),
-            "shampoo_beta": betas[2],
+            "betas": betas[:3],
             "eps": eps,
             "weight_decay": weight_decay,
             "precondition_frequency": precondition_frequency,
@@ -74,72 +73,67 @@ class SOAP(torch.optim.Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-            vals = []
+            lists = []
 
             for p in group["params"]:
                 if p.grad is None:
                     continue
 
-                grad = p.grad
-                p.grad = None
+                g = p.grad
 
                 state = self.state[p]
                 state["step"] = step = state.get("step", -1) + 1
 
                 if "exp_avg" not in state:
-                    state["exp_avg"] = torch.zeros_like(grad)
-                    state["exp_avg_sq"] = torch.zeros_like(grad)
+                    state["exp_avg"] = torch.zeros_like(g)
+                    state["exp_avg_sq"] = torch.zeros_like(g)
+
                     self.init_preconditioner(
-                        grad,
+                        g,
                         state,
                         precondition_1d=group["precondition_1d"],
                         max_precond_dim=group["max_precond_dim"],
                         merge_dims=group["merge_dims"],
                     )
-                    self.update_preconditioner(
-                        grad,
-                        state,
-                        shampoo_beta=group["shampoo_beta"],
-                        precondition_frequency=group["precondition_frequency"],
-                        precondition_warmup=group["precondition_warmup"],
-                    )
 
-                    continue  # skip first gradient step
+                    continue
 
-                # Project gradients to the eigenbases of Shampoo's preconditioner
-                grad_projected = self.project(grad, state)
+                # Project gradients to the eigenbasis of Shampoo's preconditioner
+                g_sq = self.project(g, state).square()
 
-                vals.append((p, grad, grad_projected, state["exp_avg"], state["exp_avg_sq"]))
+                lists.append((p, g, g_sq, state["exp_avg"], state["exp_avg_sq"]))
 
-            if not vals:
+            if not lists:
                 continue
 
-            params, grad, grad_projected, exp_avg, exp_avg_sq = zip(*vals, strict=True)
-            beta1, beta2 = group["betas"]
+            params, grads, grads_sq, exp_avg, exp_avg_sq = zip(*lists, strict=True)
 
-            debiased1 = (1 - beta1) / (1 - beta1**step)
-            debiased2 = (1 - beta2) / (1 - beta2**step)
+            # Moments
+            beta1, beta2, beta3 = group["betas"]
 
-            # Decay the first and second moment running average
-            torch._foreach_lerp_(exp_avg, grad, debiased1)
-            torch._foreach_mul_(exp_avg_sq, 1 - debiased2)
-            torch._foreach_addcmul_(exp_avg_sq, grad_projected, grad_projected, value=debiased2)
+            beta1_ = 1 - (1 - beta1) / (1 - beta1**step)
+            beta2_ = 1 - (1 - beta2) / (1 - beta2**step)
+            beta3_ = 1 - (1 - beta3) / (1 - beta3 ** (step + 1))
 
-            del grad_projected
+            torch._foreach_lerp_(exp_avg, grads, 1 - beta1_)
+            torch._foreach_lerp_(exp_avg_sq, grads_sq, 1 - beta2_)
 
+            del grads_sq
+
+            # Denominator
+            denom = torch._foreach_sqrt(exp_avg_sq)
+            torch._foreach_add_(denom, group["eps"])
+
+            # Update
             updates = []
 
-            for p, g, ea, ea_sq in zip(params, grad, exp_avg, exp_avg_sq, strict=True):
+            for p, g, u, d in zip(params, grads, exp_avg, denom, strict=True):
                 state = self.state[p]
 
-                # Project the exponential moving average of gradients to the eigenbases of Shampoo's preconditioner
-                u = self.project(ea, state)
-
-                # Normalize with respect to the sqrt of the exponential moving average of the squared projected gradients
-                u = u / torch.clip(torch.sqrt(ea_sq), min=group["eps"])
-
-                # Project back
-                u = self.project_back(u, state)
+                # Adam in the eigenbasis of Shampoo's preconditioner
+                u = self.project(u, state)
+                u = u / d
+                u = self.project(u, state, back=True)
 
                 updates.append(u)
 
@@ -147,7 +141,7 @@ class SOAP(torch.optim.Optimizer):
                 self.update_preconditioner(
                     g,
                     state,
-                    shampoo_beta=group["shampoo_beta"],
+                    shampoo_beta=beta3_,
                     precondition_frequency=group["precondition_frequency"],
                     precondition_warmup=group["precondition_warmup"],
                 )
@@ -181,12 +175,14 @@ class SOAP(torch.optim.Optimizer):
 
         if grad.numel() > 1 and (grad.ndim > 1 or precondition_1d):
             for s in state["precond_shape"]:
-                if s > max_precond_dim:
+                if s > max_precond_dim or s == 1:
                     state["GG"].append(None)
                 else:
                     state["GG"].append(torch.zeros(s, s, device=grad.device))
         else:
             state["GG"].append(None)
+
+        self.update_preconditioner(grad, state, shampoo_beta=0.0)
 
     def update_preconditioner(
         self,
@@ -202,100 +198,67 @@ class SOAP(torch.optim.Optimizer):
 
         for i, m in enumerate(state["GG"]):
             if m is not None:
-                outer_product = torch.tensordot(
+                outer = torch.tensordot(
                     grad,
                     grad,
                     dims=[(*range(i), *range(i + 1, grad.ndim))] * 2,
                 )
-                m.lerp_(outer_product, 1 - shampoo_beta)
+                m.lerp_(outer, 1 - shampoo_beta)
 
         if state["Q"] is None:
             state["Q"] = self.get_orthogonal_matrix(state)
+        elif state["step"] < precondition_warmup or state["step"] % precondition_frequency == 0:
+            state["Q"] = self.get_orthogonal_matrix_QR(state)
 
-        if state["step"] > 0:
-            if state["step"] < precondition_warmup or state["step"] % precondition_frequency == 0:
-                state["Q"] = self.get_orthogonal_matrix_QR(state)
-
-    def project(self, grad, state):
-        """Projects the gradient to the eigenbases of the preconditioner."""
+    def project(self, grad, state, back: bool = False):
+        """Projects the gradient to/from the eigenbasis of the preconditioner."""
 
         grad_shape = grad.shape
         grad = grad.reshape(state["precond_shape"])
 
-        for mat in state["Q"]:
-            if mat is None:
+        for q in state["Q"]:
+            if q is None:
                 grad = grad.movedim(0, -1)
             else:
                 grad = torch.tensordot(
                     grad,
-                    mat,
-                    dims=[[0], [0]],
-                )
-
-        return grad.reshape(grad_shape)
-
-    def project_back(self, grad, state):
-        """Projects the gradient back to the original space."""
-
-        grad_shape = grad.shape
-        grad = grad.reshape(state["precond_shape"])
-
-        for mat in state["Q"]:
-            if mat is None:
-                grad = grad.movedim(0, -1)
-            else:
-                grad = torch.tensordot(
-                    grad,
-                    mat,
-                    dims=[[0], [1]],
+                    q,
+                    dims=[[0], [1 if back else 0]],
                 )
 
         return grad.reshape(grad_shape)
 
     def get_orthogonal_matrix(self, state):
-        """Computes the eigenbases of the preconditioner using torch.linalg.eigh decomposition."""
+        """Computes the eigenbasis of the preconditioner using torch.linalg.eigh decomposition."""
 
-        results = []
+        Q = []
 
         for m in state["GG"]:
             if m is None:
-                results.append(None)
+                Q.append(None)
             else:
-                try:
-                    _, Q = torch.linalg.eigh(
-                        m.data.to(torch.float32) + 1e-30 * torch.eye(m.shape[0], device=m.device)
-                    )
-                except:  # noqa: E722
-                    _, Q = torch.linalg.eigh(
-                        m.data.to(torch.float64) + 1e-30 * torch.eye(m.shape[0], device=m.device)
-                    )
-                    Q = Q.to(torch.float32)
+                _, q = torch.linalg.eigh(
+                    m.to(torch.float32) + 1e-12 * torch.eye(*m.shape, device=m.device)
+                )
+                Q.append(torch.fliplr(q))
 
-                Q = torch.flip(Q, [1])
-                results.append(Q)
-
-        return results
+        return Q
 
     def get_orthogonal_matrix_QR(self, state):
-        """Computes the eigenbases of the preconditioner using one round of power
+        """Computes the eigenbasis of the preconditioner using one round of power
         iteration followed by torch.linalg.qr decomposition."""
 
-        exp_avg_sq = state["exp_avg_sq"].reshape(state["precond_shape"])
+        Q = []
 
-        results = []
-
-        for dim, (m, o) in enumerate(zip(state["GG"], state["Q"], strict=True)):
+        for m, q in zip(state["GG"], state["Q"], strict=True):
             if m is None:
-                results.append(None)
+                Q.append(None)
             else:
-                m, o = m.data.to(torch.float32), o.data.to(torch.float32)
-                mo = m @ o
-                est_eig = torch.einsum("ij,ij->j", o, mo)
-                sort_idx = torch.argsort(est_eig, descending=True)
-                exp_avg_sq = exp_avg_sq.index_select(dim, sort_idx)
-                Q, _ = torch.linalg.qr(mo[:, sort_idx])
-                results.append(Q)
+                m, q = m.to(torch.float32), q.to(torch.float32)
+                mq = m @ q
+                eigen = torch.einsum("ij,ij->j", q, mq)
+                order = torch.argsort(eigen, descending=True)
+                q[:, order], _ = torch.linalg.qr(mq[:, order])
+                Q.append(q)
 
-        state["exp_avg_sq"] = exp_avg_sq.reshape(state["exp_avg_sq"].shape)
-
-        return results
+        return Q
