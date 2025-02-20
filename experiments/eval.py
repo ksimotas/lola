887a12b8ef5dbe7a3c2a2aa39a46217c6f5d8fa5
François Rozet <francois.rozet@outlook.com>
2025-02-20 18:22:13 +0100

    Optimize eval script

--- experiments/eval.py
+++ experiments/eval.py
@@ -14,9 +14,9 @@ from lola.hydra import compose
 def evaluate(
     run: str,
     server: DictConfig,
+    indices: Sequence[Union[int, float]],
     target: str = "state",
     split: str = "valid",
-    index: Union[int, float] = 0,
     start: int = 0,
     context: int = 1,
     overlap: int = 1,
@@ -24,9 +24,7 @@ def evaluate(
     sampling: Dict[str, Any] = {},  # noqa: B006
     seed: Optional[int] = None,
     record: int = 1,
-    # Ignore
-    array: Sequence[Union[int, float]] = (),
-    compute: Optional[DictConfig] = None,
+    **ignore,
 ):
     import matplotlib.pyplot as plt
     import torch
@@ -37,6 +35,7 @@ def evaluate(
     from functools import partial
     from omegaconf import OmegaConf
     from pathlib import Path
+    from tqdm import tqdm
 
     from lola.autoencoder import get_autoencoder
     from lola.data import field_preprocess, get_well_inputs, get_well_multi_dataset
@@ -79,17 +78,6 @@ def evaluate(
         transform=cfg.dataset.transform,
     )
 
-    ## Item
-    if isinstance(index, float):
-        index = int(index * len(dataset))
-
-    x, label = get_well_inputs(dataset[index], device=device)
-    x = x[start :: cfg.trajectory.stride]
-    x = preprocess(x)
-    x = rearrange(x, "L H W C -> C L H W")
-
-    labels = label.tolist()
-
     # Autoencoder
     if (runpath / "autoencoder").exists():
         cfg.ae = OmegaConf.load(runpath / "autoencoder/config.yaml").ae
@@ -112,19 +100,23 @@ def evaluate(
         autoencoder.encode = nn.Identity()
         autoencoder.decode = nn.Identity()
 
-    ## Encode
+    ## Get the latent shape and compression ratio
+    x, label = get_well_inputs(dataset[0], device=device)
+    x = x[start :: cfg.trajectory.stride]
+    x = preprocess(x)
+    x = rearrange(x, "L H W C -> C L H W")
+
     with torch.no_grad():
         z = encode_traj(autoencoder, x)
-        x_ae = decode_traj(autoencoder, z)
 
+    traj_shape = (z.shape[0], cfg.trajectory.length, *z.shape[2:])
     compression = x.numel() / z.numel()
 
     # Emulator
-    shape = (z.shape[0], cfg.trajectory.length, *z.shape[2:])
-
     if hasattr(cfg, "denoiser"):
+        method = "diffusion"
         denoiser = get_denoiser(
-            shape=shape,
+            shape=traj_shape,
             label_features=label.numel(),
             masked=True,
             **cfg.denoiser,
@@ -135,9 +127,18 @@ def evaluate(
         )
         denoiser.to(device)
         denoiser.eval()
+
+        emulate = lambda mask, z_obs, label: emulate_diffusion(
+            denoiser,
+            mask,
+            z_obs,
+            label=label,
+            **sampling,
+        )
     elif hasattr(cfg, "surrogate"):
+        method = "surrogate"
         surrogate = get_surrogate(
-            shape=shape,
+            shape=traj_shape,
             label_features=label.numel(),
             **cfg.surrogate,
         )
@@ -148,127 +149,129 @@ def evaluate(
         surrogate.to(device)
         surrogate.eval()
 
-    ## RNG
-    if seed is None:
-        seed = torch.initial_seed()
-
-    _ = torch.manual_seed(seed + 101 * index + start)
-
-    ## Emulation
-    if hasattr(cfg, "denoiser"):
-        method = "diffusion"
-        emulate = lambda mask, z_obs: emulate_diffusion(
-            denoiser,
-            mask,
-            z_obs,
-            label=label,
-            **sampling,
-        )
-    elif hasattr(cfg, "surrogate"):
-        method = "surrogate"
-        emulate = lambda mask, z_obs: emulate_surrogate(
+        emulate = lambda mask, z_obs, label: emulate_surrogate(
             surrogate,
             mask,
             z_obs,
             label=label,
         )
 
-    z_hat = emulate_rollout(
-        emulate,
-        z,
-        window=cfg.trajectory.length,
-        rollout=z.shape[1],
-        context=(context - 1) // cfg.trajectory.stride + 1,
-        overlap=overlap,
-        batch=samples,
-    )
-
-    with torch.no_grad():
-        x_hat = decode_traj(autoencoder, z_hat, batched=True)
-
-    del z_hat
+    # RNG
+    if seed is None:
+        seed = torch.initial_seed()
 
     # Evaluation
-    lines = []
+    for index in tqdm(indices, ncols=88, ascii=True):
+        if isinstance(index, float):
+            index = int(index * len(dataset))
 
-    for field in range(x.shape[0]):
-        for t in range(x.shape[1]):
-            for auto_encoded in (False, True):
-                if auto_encoded:
-                    u, v = x_ae[field, t], x_hat[:, field, t]
-                else:
-                    u, v = x[field, t], x_hat[:, field, t]
+        _ = torch.manual_seed(seed + 101 * index + start)
 
-                # Spread
-                if samples > 1:
-                    spread = torch.sqrt(torch.mean(torch.square(v - torch.mean(v, dim=0))))
-                else:
-                    spread = 0.0
+        x, label = get_well_inputs(dataset[index], device=device)
+        x = x[start :: cfg.trajectory.stride]
+        x = preprocess(x)
+        x = rearrange(x, "L H W C -> C L H W")
 
-                # Skill
-                se = torch.square(u - torch.mean(v, dim=0))
-                mse = torch.mean(se)
-                rmse = torch.sqrt(mse)
-                nrmse = torch.sqrt(mse / torch.mean(u**2))
-                vrmse = torch.sqrt(mse / torch.var(u))
+        with torch.no_grad():
+            z = encode_traj(autoencoder, x)
+            x_ae = decode_traj(autoencoder, z)
 
-                # Fourier
-                p_u, k = isotropic_power_spectrum(u, spatial=2)
-                p_v, _ = isotropic_power_spectrum(v, spatial=2)
-                p_v = torch.mean(p_v, dim=0)
-                c_uv, _ = isotropic_cross_correlation(u, v, spatial=2)
-                c_uv = torch.mean(c_uv, dim=0)
+        z_hat = emulate_rollout(
+            partial(emulate, label=label),
+            z,
+            window=cfg.trajectory.length,
+            rollout=z.shape[1],
+            context=context,
+            overlap=overlap,
+            batch=samples,
+        )
 
-                sre_p = torch.square(1 - p_v / p_u)
-                sre_c = torch.square(1 - c_uv / torch.sqrt(p_u * p_v))
+        with torch.no_grad():
+            x_hat = decode_traj(autoencoder, z_hat, batched=True)
 
-                rmsre_f = []
+        del z_hat
 
-                bins = torch.logspace(k[0].log2(), -1.0, steps=4, base=2)
+        ## Stats
+        lines = []
 
-                for i in range(4):
-                    if i < 3:
-                        mask = torch.logical_and(bins[i] <= k, k <= bins[i + 1])
+        for field in range(x.shape[0]):
+            for t in range(x.shape[1]):
+                for auto_encoded in (False, True):
+                    if auto_encoded:
+                        u, v = x_ae[field, t], x_hat[:, field, t]
                     else:
-                        mask = bins[i] <= k
+                        u, v = x[field, t], x_hat[:, field, t]
 
-                    rmsre_f.append(torch.sqrt(torch.mean(sre_p[mask])))
-                    rmsre_f.append(torch.sqrt(torch.mean(sre_c[mask])))
+                    # Spread
+                    if samples > 1:
+                        spread = torch.sqrt(torch.mean(torch.square(v - torch.mean(v, dim=0))))
+                    else:
+                        spread = 0.0
 
-                # Write
-                line = f"{runname},{target},{method},{compression},"
-                line += f"{split},{index},{start},{seed},{context},{overlap},{auto_encoded},{field},{t * cfg.trajectory.stride},"
-                line += f"{spread},{rmse},{nrmse},{vrmse},"
-                line += ",".join(map(format, (*rmsre_f, *labels)))
-                line += "\n"
+                    # Skill
+                    se = torch.square(u - torch.mean(v, dim=0))
+                    mse = torch.mean(se)
+                    rmse = torch.sqrt(mse)
+                    nrmse = torch.sqrt(mse / torch.mean(u**2))
+                    vrmse = torch.sqrt(mse / torch.var(u))
 
-                lines.append(line)
+                    # Fourier
+                    p_u, k = isotropic_power_spectrum(u, spatial=2)
+                    p_v, _ = isotropic_power_spectrum(v, spatial=2)
+                    p_v = torch.mean(p_v, dim=0)
+                    c_uv, _ = isotropic_cross_correlation(u, v, spatial=2)
+                    c_uv = torch.mean(c_uv, dim=0)
 
-    outdir = Path(f"{server.storage}/results/{cfg.dataset.name}")
-    outdir = outdir.expanduser().resolve()
-    outdir.mkdir(parents=True, exist_ok=True)
+                    sre_p = torch.square(1 - p_v / p_u)
+                    sre_c = torch.square(1 - c_uv / torch.sqrt(p_u * p_v))
 
-    with FileLock(outdir / "stats.csv.lock"):
-        with open(outdir / "stats.csv", mode="a") as f:
-            f.writelines(lines)
+                    rmsre_f = []
 
-    # Video
-    plt.rcParams["animation.ffmpeg_path"] = "/mnt/sw/nix/store/fz8y69w4c97lcgv1wwk03bd4yh4zank7-ffmpeg-full-6.0-bin/bin/ffmpeg"  # fmt: off
+                    bins = torch.logspace(k[0].log2(), -1.0, steps=4, base=2)
 
-    if x.shape[-1] < x.shape[-2]:
-        x, x_hat = x.mT, x_hat.mT
+                    for i in range(4):
+                        if i < 3:
+                            mask = torch.logical_and(bins[i] <= k, k <= bins[i + 1])
+                        else:
+                            mask = bins[i] <= k
 
-    for i in range(min(samples, record)):
-        animation = animate_fields(
-            x,
-            x_hat[i],
-            fields=cfg.dataset.fields,
-            figsize=(x.shape[-1] / 64, x.shape[-2] / 64),
-        )
-        animation.save(
-            outdir
-            / f"{runname}_{target}_{split}_{index:06d}_{start:03d}_{seed}_{context}_{overlap}_{i:03d}.mp4"
-        )
+                        rmsre_f.append(torch.sqrt(torch.mean(sre_p[mask])))
+                        rmsre_f.append(torch.sqrt(torch.mean(sre_c[mask])))
+
+                    # Write
+                    line = f"{runname},{target},{method},{compression},"
+                    line += f"{split},{index},{start},{seed},{(context - 1) * cfg.trajectory.stride + 1},{overlap},{auto_encoded},{field},{(t - context) * cfg.trajectory.stride},"
+                    line += f"{spread},{rmse},{nrmse},{vrmse},"
+                    line += ",".join(map(format, (*rmsre_f, *label.tolist())))
+                    line += "\n"
+
+                    lines.append(line)
+
+        outdir = Path(f"{server.storage}/results/{cfg.dataset.name}")
+        outdir = outdir.expanduser().resolve()
+        outdir.mkdir(parents=True, exist_ok=True)
+
+        with FileLock(outdir / "stats.csv.lock"):
+            with open(outdir / "stats.csv", mode="a") as f:
+                f.writelines(lines)
+
+        # Video
+        plt.rcParams["animation.ffmpeg_path"] = "/mnt/sw/nix/store/fz8y69w4c97lcgv1wwk03bd4yh4zank7-ffmpeg-full-6.0-bin/bin/ffmpeg"  # fmt: off
+
+        if x.shape[-1] < x.shape[-2]:
+            x, x_hat = x.mT, x_hat.mT
+
+        for i in range(min(samples, record)):
+            animation = animate_fields(
+                x,
+                x_hat[i],
+                fields=cfg.dataset.fields,
+                figsize=(x.shape[-1] / 64, x.shape[-2] / 64),
+            )
+            animation.save(
+                outdir
+                / f"{runname}_{target}_{split}_{index:06d}_{start:03d}_{seed}_{context}_{overlap}_{i:03d}.mp4"
+            )
 
 
 if __name__ == "__main__":
@@ -294,13 +297,13 @@ if __name__ == "__main__":
 
     # Job
     def launch(i: int):
-        evaluate(index=array[i], **cfg)
+        evaluate(indices=array[i :: cfg.compute.jobs], **cfg)
 
     dawgz.schedule(
         dawgz.job(
             f=launch,
             name="eval",
-            array=len(array),
+            array=cfg.compute.jobs,
             cpus=cfg.compute.cpus,
             gpus=cfg.compute.gpus,
             ram=cfg.compute.ram,
