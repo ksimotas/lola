5c459769e3853cd4d689807a1bd846d0521855ea
François Rozet <francois.rozet@outlook.com>
2025-08-18 11:29:08 +0200

    Update Rayleigh-Bénard runs

--- experiments/eval_all.sh
+++ experiments/eval_all.sh
@@ -5,9 +5,9 @@ runs=(
     "$HOME/ceph/lola/runs/ae/z522jsw5_euler_all_dcae_f32c4_large"
     "$HOME/ceph/lola/runs/ae/8f21pxzs_euler_all_dcae_f32c16_large"
     "$HOME/ceph/lola/runs/ae/n6g8kix8_euler_all_dcae_f32c64_large"
-    "$HOME/ceph/lola/runs/ae/2bkdxjib_rayleigh_benard_dcae_f32c4_large"
-    "$HOME/ceph/lola/runs/ae/84hwskd9_rayleigh_benard_dcae_f32c16_large"
-    "$HOME/ceph/lola/runs/ae/di2j3rpb_rayleigh_benard_dcae_f32c64_large"
+    "$HOME/ceph/lola/runs/ae/8brbetky_rayleigh_benard_dcae_f32c4_large"
+    "$HOME/ceph/lola/runs/ae/jiof10wl_rayleigh_benard_dcae_f32c16_large"
+    "$HOME/ceph/lola/runs/ae/1e3z5x2c_rayleigh_benard_dcae_f32c64_large"
     "$HOME/ceph/lola/runs/ae/gf09iy5g_gravity_cooling_dcae_3d_f8c4_large"
     "$HOME/ceph/lola/runs/ae/0zc7afa4_gravity_cooling_dcae_3d_f8c16_large"
     "$HOME/ceph/lola/runs/ae/dnzp6wv7_gravity_cooling_dcae_3d_f8c64_large"
@@ -15,9 +15,9 @@ runs=(
     "$HOME/ceph/lola/runs/dm/otocl205_euler_all_f32c4_vit_large"
     "$HOME/ceph/lola/runs/dm/oy7s3wfq_euler_all_f32c16_vit_large"
     "$HOME/ceph/lola/runs/dm/e7xzovde_euler_all_f32c64_vit_large"
-    "$HOME/ceph/lola/runs/dm/cyqmbfjb_rayleigh_benard_f32c4_vit_large"
-    "$HOME/ceph/lola/runs/dm/667t8kzm_rayleigh_benard_f32c16_vit_large"
-    "$HOME/ceph/lola/runs/dm/0fqjt3js_rayleigh_benard_f32c64_vit_large"
+    "$HOME/ceph/lola/runs/dm/6hsjhmvw_rayleigh_benard_f32c4_vit_large"
+    "$HOME/ceph/lola/runs/dm/e0sdjqy9_rayleigh_benard_f32c16_vit_large"
+    "$HOME/ceph/lola/runs/dm/ny04m1tl_rayleigh_benard_f32c64_vit_large"
     "$HOME/ceph/lola/runs/dm/6xekmygr_gravity_cooling_f8c4_vit_large"
     "$HOME/ceph/lola/runs/dm/2cxh0m26_gravity_cooling_f8c16_vit_large"
     "$HOME/ceph/lola/runs/dm/5cywlsx8_gravity_cooling_f8c64_vit_large"
@@ -25,20 +25,20 @@ runs=(
     "$HOME/ceph/lola/runs/sm/a3hc2as6_euler_all_f32c4_vit_large"
     "$HOME/ceph/lola/runs/sm/95zfkk6w_euler_all_f32c16_vit_large"
     "$HOME/ceph/lola/runs/sm/s259b4l6_euler_all_f32c64_vit_large"
-    "$HOME/ceph/lola/runs/sm/chua0k42_rayleigh_benard_f32c4_vit_large"
-    "$HOME/ceph/lola/runs/sm/xp1prkdo_rayleigh_benard_f32c16_vit_large"
-    "$HOME/ceph/lola/runs/sm/lrg1qgi2_rayleigh_benard_f32c64_vit_large"
+    "$HOME/ceph/lola/runs/sm/xqm575ny_rayleigh_benard_f32c4_vit_large"
+    "$HOME/ceph/lola/runs/sm/yubzc3d1_rayleigh_benard_f32c16_vit_large"
+    "$HOME/ceph/lola/runs/sm/2k83f6km_rayleigh_benard_f32c64_vit_large"
     "$HOME/ceph/lola/runs/sm/5haia87a_gravity_cooling_f8c4_vit_large"
     "$HOME/ceph/lola/runs/sm/5rtbuoht_gravity_cooling_f8c16_vit_large"
     "$HOME/ceph/lola/runs/sm/e21q2faa_gravity_cooling_f8c64_vit_large"
     "$HOME/ceph/lola/runs/sm/1y8qrgee_euler_all_pixel_vit_pixel"
-    "$HOME/ceph/lola/runs/sm/ses7lyf5_rayleigh_benard_pixel_vit_pixel"
+    "$HOME/ceph/lola/runs/sm/43iu4768_rayleigh_benard_pixel_vit_pixel"
 )
 
 for run in "${runs[@]}"
 do
     if [[ $run = *"rayleigh_benard"* ]]; then
-        starts=(8 16)
+        starts=(16)
     elif [[ $run = *"gravity_cooling"* ]]; then
         starts=(4)
     else
